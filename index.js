3d821c30ff2d417e1ad591e802574a9ec2f08aa4
abnish123kumar <103102072+abnish123kumar@users.noreply.github.com>
2023-04-10 10:27:09 +0530

    server

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+const mysql = require('mysql');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const db = mysql.createConnection({
+    host :"localhost",
+    user : 'root',
+    password : '',
+    database : "abnish_db"
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/data',(req,res)=>{
+    db.query("Select * from train",(err,result)=>{
+        if(err){
+            throw err;
+        }else{
+            res.send(result);
+        }
+    })
+});
+app.put('/data',(req,res)=>{
+    let obj = req.body;
+    console.log(obj)
+    db.query("UPDATE train SET ? WHERE seat="+obj.seat,[obj],(err,result)=>{
+        if(err){
+
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+
+const Port = process.env.PORT || 4500;
+app.listen(Port,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("server is running");
+    }
+})
